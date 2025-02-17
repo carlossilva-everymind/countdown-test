ac6bb238ff597140f57fef3c1830fb255ec336e1
carlossilva-everymind <carlos.silva@everymind.com.br>
2025-02-17 15:20:29 -0300

    adjust 5

--- gif.php
+++ gif.php
@@ -3,76 +3,46 @@
 date_default_timezone_set('America/Sao_Paulo');
 include 'GIFEncoder.class.php';
 include 'php52-fix.php';
-$time = $_GET['time'];
-$future_date = new DateTime(date('r',strtotime($time)));
-$time_now = time();
-$now = new DateTime(date('r', $time_now));
-$frames = array();    
-$delays = array();
+$future_date = new DateTime(date('r', strtotime($time)));
+$now = new DateTime(date('r', time()));
 
-$delay = 100;// milliseconds
-
-$font = array(
-    'size' => 40, // Font size, in pts usually.
-    'angle' => 0, // Angle of the text
-    'x-offset' => 70, // The larger the number the further the distance from the left hand side, 0 to align to the left.
-    'y-offset' => 120, // The vertical alignment, trial and error between 20 and 60.
-    'file' => __DIR__ . DIRECTORY_SEPARATOR . 'Arial Narrow.otf', // Font path
-);
-
-for($i = 0; $i <= 60; $i++){
+for ($i = 0; $i <= 60; $i++) {
+    $interval = $future_date->diff($now);
     
-    $interval = date_diff($future_date, $now);
-    
-    // Open the first source image and add the text.
-    $image = imagecreatefrompng('images/bg.png');
-    $font['color'] = imagecolorallocate($image, 255,255,255); // Create color after image
-    
-    if($future_date < $now){
-        $text = $interval->format('00:00:00:00');
-        imagettftext ($image , $font['size'] , $font['angle'] , $font['x-offset'] , $font['y-offset'] , $font['color'] , $font['file'], $text );
-        ob_start();
-        imagegif($image);
-        $frames[]=ob_get_contents();
-        $delays[]=$delay;
-        $loops = 1;
-        ob_end_clean();
-        break;
+    if ($future_date->getTimestamp() < $now->getTimestamp()) {
+        $text = '00:00:00:00';
     } else {
-        $hour = $interval->format('%H');
-        $day = $interval->format('%D');
-        $minute = $interval->format('%I');
-        $second = $interval->format('%S');
+        $day = $interval->days;
+        $hour = $interval->h;
+        $minute = $interval->i;
+        $second = $interval->s;
+        
         if ($day > 0) {
             $hour += 1;
         }
-        if (strlen($hour) == 1) {
-            $hour = '0'.$hour;
-            
-        } 
-        $text = $interval->format( '      '.$day.'   '.$hour.'   '.$minute.'    '.$second);
-        imagettftext ($image , $font['size'] , $font['angle'] , $font['x-offset'] , $font['y-offset'] -70 , $font['color'] , $font['file'], $text );
 
-        // Add labels
-        $labels = "                  Dias          Horas       Minutos     Segundos";
-        imagettftext ($image , $font['size'] -25 , $font['angle'] , $font['x-offset'] -10  , $font['y-offset'] -50 , $font['color'] , $font['file'], $labels );
-
-        ob_start();
-        imagegif($image);
-        $frames[]=ob_get_contents();
-        $delays[]=$delay;
-        $loops = 0;
-        ob_end_clean();
+        $text = sprintf("      %02d   %02d   %02d    %02d", $day, $hour, $minute, $second);
     }
+
+    $image = imagecreatefrompng('images/bg.png');
+    $font['color'] = imagecolorallocate($image, 255, 255, 255);
+    
+    imagettftext($image, $font['size'], $font['angle'], $font['x-offset'], $font['y-offset'] - 70, $font['color'], $font['file'], $text);
+    
+    ob_start();
+    imagegif($image);
+    $frames[] = ob_get_contents();
+    $delays[] = 100;
+    ob_end_clean();
+    
+    imagedestroy($image);
 
     $now->modify('+1 second');
 }
 
-//expire this image instantly
-header( 'Expires: Sat, 26 Jul 1997 05:00:00 GMT' );
-header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT' );
-header( 'Cache-Control: no-store, no-cache, must-revalidate' );
-header( 'Cache-Control: post-check=0, pre-check=0', false );
-header( 'Pragma: no-cache' );
-$gif = new AnimatedGif($frames,$delays,$loops);
+header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Pragma: no-cache');
+
+$gif = new AnimatedGif($frames, $delays, 0);
 $gif->display();
